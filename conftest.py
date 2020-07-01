5d89a9851b8927a129445bd96096468c103b4684
Rodeni4 <65849224+Rodeni4@users.noreply.github.com>
2020-07-01 15:38:29 +0700

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from selenium import webdriver
 
 @pytest.fixture(scope="function")
 def browser():
@@ -9,8 +8,3 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
-    browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
