3e4f9d35fa28b26ba4793cc8f85359201fe9ff33
Rodeni4 <rodenkosv@gmail.com>
2020-07-02 17:14:48 +0700

    Загрузка урока 3.6

--- conftest.py
+++ conftest.py
@@ -1,10 +1,14 @@
 import pytest
+from selenium import webdriver
 
-@pytest.fixture(scope="function")
+
+@pytest.fixture
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
     yield browser
+    # этот код выполнится после завершения теста
     print("\nquit browser..")
     browser.quit()
+
 
--- test_items.py
+++ test_items.py
@@ -1,8 +1,11 @@
 import pytest
+import time
 from selenium import webdriver
+
 
 @pytest.mark.parametrize('language', ["ru", "en-gb"])
 def test_guest_should_see_login_link(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
     browser.get(link)
+    time.sleep(30)
     browser.find_element_by_css_selector("#login_link")
