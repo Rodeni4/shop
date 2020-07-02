042258ce6b54a05e7bfe9838c80ac538f9d4683e
Rodeni4 <rodenkosv@gmail.com>
2020-07-02 20:31:25 +0700

    Добавление коментариев

--- conftest.py
+++ conftest.py
@@ -1,14 +1,22 @@
 import pytest
 from selenium import webdriver
 
-
+# если добавить (scope="module") браузер запустится и закроется один раз
 @pytest.fixture
 def browser():
-    print("\nstart browser for test..")
+    print("\nЗапускается браузер для тестирования..")
     browser = webdriver.Chrome()
+    #return browser
+
     yield browser
     # этот код выполнится после завершения теста
-    print("\nquit browser..")
-    browser.quit()
+    print("\nЗакрытие браузера..")
+    browser.quit() \
+
+# если добавить (autouse=True) фикстура запустится для каждого теста даже без явного вызова
+@pytest.fixture
+def prepare_data():
+    print()
+    print("Подготовка некоторых критических данных для каждого теста")
 
 
--- test_items.py
+++ test_items.py
@@ -4,8 +4,9 @@ from selenium import webdriver
 
 
 @pytest.mark.parametrize('language', ["ru", "en-gb"])
+# вызываем фикстуру в тесте, передав ее как параметр
 def test_guest_should_see_login_link(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
     browser.get(link)
-    time.sleep(30)
+    time.sleep(10)
     browser.find_element_by_css_selector("#login_link")
