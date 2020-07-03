0fb0e28c8b74d998b32de24e2b907c26db441fec
Rodeni4 <rodenkosv@gmail.com>
2020-07-03 15:07:18 +0700

    Готов к проверке!

--- test_items.py
+++ test_items.py
@@ -7,8 +7,8 @@ def test_btn_add_to_basket(browser):
     try:
         browser.get(link)
         time.sleep(30)
-        browser.find_element_by_class_name("btn-add-t-basket")
+        browser.find_element_by_class_name("btn-add-to-basket")
         result = True
     except:
         result = False
-    assert result == True, "кнопка не найдена"
+    assert result == True, "Кнопка не найдена!"
